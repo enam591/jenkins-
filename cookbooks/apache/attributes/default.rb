69668a01ea3dfa2b293416d50d660d19242cb2b3
root <root@enam591.mylabserver.com>
2016-04-30 18:46:31 +0000

    first commit

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,4 @@
+#default["apache"]["sites"]["enam592"] = { "port" => 80, "domain" => "enam592.mylabserver.com" }
+#default["apache"]["sites"]["enam592b"] = { "port" => 80, "domain" => "enam592b.mylabserver.com" }
+default["apache"]["sites"]["enam592"] = { "site_title" => "enam592 website coming soon", "port" => 80, "domain" => "enam592.mylabserver.com" }
+default["apache"]["sites"]["enam592b"] = { "site_title" => "enam592b website coming soon!", "port" => 80, "domain" => "enam592b.mylabserver.com" }
